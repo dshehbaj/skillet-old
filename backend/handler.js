a2da75277ad2554c37998c38ed29669f1476b9c1
dshehbaj <shehbaj.dhillon@gmail.com>
2021-07-16 11:10:32 -0700

    removed starter function.

--- backend/handler.js
+++
@@ -1,7 +0,0 @@
-import handler from './libs/handler';
-
-export const main = handler(async (event, context) => {
-  const apiKey = process.env.spoonacularAPIKey;
-
-  return JSON.stringify(apiKey);
-});
