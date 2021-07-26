d918714c1daa6fd311c6c6e9b67da353962c0e49
dshehbaj <shehbaj.dhillon@gmail.com>
2021-07-25 23:08:53 -0700

    Changes to RecipeScreen.

--- frontend/screens/RecipeScreen.js
+++ frontend/screens/RecipeScreen.js
@@ -1,12 +1,14 @@
-import React from "react";
-import { Text, View, ScrollView } from "react-native";
-import { Button, Card, Title, Paragraph } from "react-native-paper";
+import React, { useState, useEffect } from "react";
+import { Text, View, ScrollView, Linking } from "react-native";
+import { List, Button, Card, Title, Paragraph } from "react-native-paper";
 import { API } from "aws-amplify";
 import RecipeCard from "../components/RecipeCard";
-import CenterView from "../components/CenterView";
 
 export default function ({ navigation, route }) {
   const { data } = route.params;
+
+  const [info, setInfo] = useState(null);
+  const [steps, setSteps] = useState(null);
 
   const openInstructions = (data) => {
     navigation.navigate("Instructions", { data: data });
@@ -15,6 +17,22 @@ export default function ({ navigation, route }) {
   const openSimilar = (data) => {
     navigation.navigate("Similar", { data: data });
   };
+
+  const getSteps = async () => {
+    let result = await API.get("skillet", `/recipes/search/${data.id}`);
+    setSteps(result);
+  };
+
+  const getInfo = async () => {
+    let result = await API.get("skillet", `/recipes/info/${data.id}`);
+    setInfo(result);
+  };
+
+  useEffect(() => {
+    getSteps();
+    getInfo();
+    console.log(info);
+  }, []);
 
   return (
     <View style={{ flex: 1 }}>
@@ -25,22 +43,44 @@ export default function ({ navigation, route }) {
           openSimilar={openSimilar}
           openInstructions={openInstructions}
         />
-        <CenterView>
-          <Text>Here you can see information on {data.title}</Text>
-        </CenterView>
-        {data.ingredients.map(({ unit, amount, name, image }) => {
-          return (
-            <Card style={{ margin: "5%", borderRadius: 0 }}>
-              <Card.Content>
-                <Paragraph style={{ textTransform: "capitalize" }}>
-                  {`${amount} ${unit} ${name}`}
-                </Paragraph>
-              </Card.Content>
-              <Card.Cover source={{ uri: image }} />
-            </Card>
-          );
-        })}
+        {info && (
+          <Card style={{ margin: "5%", borderRadius: 5 }}>
+            <Card.Content>
+              <Paragraph style={{ textAlign: "center" }}>
+                <Button icon="clock-time-ten">{info.time}</Button>
+                <Button icon="account-multiple">{info.servings}</Button>
+                <Button icon="currency-usd">
+                  {(info.pricePerServing * info.servings) / 100}
+                </Button>
+              </Paragraph>
+            </Card.Content>
+          </Card>
+        )}
+        {info && (
+          <List.Section style={{ margin: "5%", borderRadius: 5 }}>
+            <List.Accordion title="Nutrients">
+              <List.Item title="nutrients" />
+            </List.Accordion>
+          </List.Section>
+        )}
+        <List.Section style={{ margin: "5%", borderRadius: 5 }}>
+          <List.Accordion title="Ingredients">
+            {data.ingredients.map(({ unit, amount, name, image }) => {
+              return (
+                <Card style={{ margin: "5%", borderRadius: 5 }}>
+                  <Card.Content>
+                    <Paragraph style={{ textTransform: "capitalize" }}>
+                      {`${amount} ${unit} ${name}`}
+                    </Paragraph>
+                  </Card.Content>
+                  <Card.Cover source={{ uri: image }} />
+                </Card>
+              );
+            })}
+          </List.Accordion>
+        </List.Section>
       </ScrollView>
+      <Button onPress={() => Linking.openURL(info.source)}>Source</Button>
     </View>
   );
 }
